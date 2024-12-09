3d9ea9e193996f93803895bfac14fc47bb26b96a
os170524 <24hs204@s.bgu.ac.jp>
2024-12-09 13:10:27 +0900

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@
                 result = "末吉";
             } else if (colour === "黄色" && number === 1) {
                 result = "小吉";    
-        　  } else if (colour === "黄色" && number === 2) {
+        　  } else (colour === "黄色" && number === 2) {
                 result = "吉";
 
             document.getElementById('result-output').innerText = "あなたの今週の運勢は【" + result + "】です";
