7504c9ec560f940e799a6234e9accb693a1117a7
os170524 <24hs204@s.bgu.ac.jp>
2024-12-16 13:26:02 +0900

    Update app.js

--- app.js
+++ app.js
@@ -5,21 +5,21 @@
 
             if (colour === "勉強" && number === 0) {
                 result = "頭が悪い";
-            } else if (colour === "勉強" && number === 1) {
+            } else if (colour === "勉強" && number === 4) {
                 result = "頭がいい";
-            } else if (colour === "勉強" && number === 2) {
+            } else if (colour === "勉強" && number === 8) {
                 result = "秀才";
             } else if (colour === "運動" && number === 0) {
-                result = "中吉";
-            } else if (colour === "運動" && number === 1) {
-                result = "中吉";
-            } else if (colour === "運動" && number === 2) {
-                result = "大凶";
+                result = "運動不足";
+            } else if (colour === "運動" && number === 4) {
+                result = "動ける身体を持っている人";
+            } else if (colour === "運動" && number === 8) {
+                result = "スポーツマン";
             } else if (colour === "オレンジ" && number === 0) {
                 result = "凶";
-            } else if (colour === "オレンジ" && number === 1) {
+            } else if (colour === "オレンジ" && number === 4) {
                 result = "大吉";
-            } else if (colour === "オレンジ" && number === 2) {
+            } else if (colour === "オレンジ" && number === 8) {
                 result = "中吉";
             }
                 
