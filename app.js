0b692fdd41edb34b552a328e0b7ceb9678ce74d3
os170524 <24hs204@s.bgu.ac.jp>
2024-12-16 13:45:15 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
         function checkFortune() {
             var colour = document.getElementById('colour-select').value;
             var number = parseInt(document.getElementById('number-input').value, 10);
-            var result = "ただの人"; // デフォルトの値
+            var result = "ただの人で"; // デフォルトの値
 
             if (colour === "勉強" && number === 0) {
-                result = "頭が悪い";
+                result = "頭が悪いで";
             } else if (colour === "勉強" && number === 4) {
-                result = "頭がいい";
+                result = "頭がいいで";
             } else if (colour === "勉強" && number === 8) {
-                result = "秀才";
+                result = "秀才で";
             } else if (colour === "運動" && number === 0) {
-                result = "運動不足";
+                result = "運動不足で";
             } else if (colour === "運動" && number === 4) {
-                result = "動ける身体を持っている人";
+                result = "動ける身体を持っていま";
             } else if (colour === "運動" && number === 8) {
-                result = "スポーツマン";
-            } else if (colour === "オレンジ" && number === 0) {
-                result = "凶";
-            } else if (colour === "オレンジ" && number === 4) {
-                result = "大吉";
-            } else if (colour === "オレンジ" && number === 8) {
-                result = "中吉";
+                result = "スポーツマンで";
+            } else if (colour === "睡眠" && number === 0) {
+                result = "死にま";
+            } else if (colour === "睡眠" && number === 4) {
+                result = "寝不足で";
+            } else if (colour === "睡眠" && number === 8) {
+                result = "健康体で";
             }
                 
 
-            document.getElementById('result-output').innerText = "あなたは【" + result + "】です";
+            document.getElementById('result-output').innerText = "あなたは【" + result + "】す";
         }
