423e4766d62365845531bdbad375283c4725c59a
os170524 <24hs204@s.bgu.ac.jp>
2024-12-16 13:14:08 +0900

    Update app.js

--- app.js
+++ app.js
@@ -5,15 +5,15 @@
 
             if (colour === "勉強" && number === 0) {
                 result = "頭が悪い";
-            } else if (colour === "黒" && number === 1) {
+            } else if (colour === "勉強" && number === 1) {
+                result = "頭がいい";
+            } else if (colour === "勉強" && number === 2) {
+                result = "秀才";
+            } else if (colour === "運動" && number === 0) {
                 result = "中吉";
-            } else if (colour === "黒" && number === 2) {
-                result = "凶";
-            } else if (colour === "白" && number === 0) {
+            } else if (colour === "運動" && number === 1) {
                 result = "中吉";
-            } else if (colour === "白" && number === 1) {
-                result = "中吉";
-            } else if (colour === "白" && number === 2) {
+            } else if (colour === "運動" && number === 2) {
                 result = "大凶";
             } else if (colour === "オレンジ" && number === 0) {
                 result = "凶";
